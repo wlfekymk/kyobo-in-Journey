f63e7078fca51383e821c6cc3df8058913fe4142
이로얌 <wlfekymk@gmail.com>
2023-07-03 17:24:38 +0900

    Initial commit

---
+++ src/main/java/com/kyobo/kyoboinjourney/KyoboInJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.kyobo.kyoboinjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyoboInJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyoboInJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyobo/kyoboinjourney/KyoboInJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyobo.kyoboinjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyoboInJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
